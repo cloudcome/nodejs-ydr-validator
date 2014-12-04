ae709622129a3f9648dcfc82903d734c9f80a8ae
cloudcome <cloudcome@163.com>
2014-12-04 15:20:01 +0800

    validator

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var udf;
 var defaults = {
     // 是否在遇到非法就中断，默认false
     // true: 返回单个错误对象
-    // false: 返回错误对象组成的对象
+    // false: 返回错误对象组成的数组
     isBreakOnInvalid: false
 };
 var Validator = klass.create({
@@ -62,71 +62,71 @@ var Validator = klass.create({
      *
      * @example
      * validator.pushRule({
-         *    // 字段名称，必须，唯一性
-         *    name: 'username',
-         *    // 数据类型，必须，为 string/email/url/number/boolean 之一
-         *    type: 'string',
-         *    // 别称，当没有填写自定义错误消息时
-         *    // 提示为 username不能为空
-         *    // 当前设置别名之后
-         *    // 提示为 用户名不能为空
-         *    alias: '用户名',
-         *
-         *    // 验证前置
-         *    // val 当前字段值
-         *    // data 所有数据
-         *    onbefore: function(val, data){
-         *        return val + 'abc';
-         *    },
-         *    exist: false,
-         *    trim: true,
-         *
-         *    // 验证规则
-         *    required: true,
-         *    length: 10,
-         *    minLength: 4,
-         *    maxLength: 12,
-         *    bytes: 10,
-         *    minBytes: 4,
-         *    maxBytes: 12,
-         *    min: 4,
-         *    max: 4,
-         *    regexp: /^[a-z]\w{3,11}$/,
-         *    equal: 'cloudcome',
-         *    inArray: ['cloudcome', 'yundanran'],
-         *    // val 当前字段值
-         *    // [data 所有数据] 可选
-         *    // next 执行下一步
-         *    function: function(val, next){
-         *        // 这里可以是异步的
-         *        // 比如远程校验可以写在这里
-         *        ajax.post('./validate.json', {
-         *            username: val
-         *        }).on('success', function(json){
-         *            if(json.code>0){
-         *                next();
-         *            }else{
-         *                next(new Error(json.msg));
-         *            }
-         *        }).on('error', function(){
-         *            next(new Error('网络连接错误，验证失败'));
-         *        });
-         *    },
-         *
-         *    // 验证消息
-         *    msg: {
-         *        required: '用户名不能为空',
-         *        length: '用户名长度必须为10'
-         *        // 未自定义的消息，以默认输出
-         *        // 每个验证规则都必须配备一个消息体，
-         *        // 除了自定义的`function`
-         *    },
-         *
-         *    // 验证后置
-         *    onafter: function(val){
-         *        return val + 'abc';
-         *    }
-         * });
+     *    // 字段名称，必须，唯一性
+     *    name: 'username',
+     *    // 数据类型，必须，为 string/email/url/number/boolean 之一
+     *    type: 'string',
+     *    // 别称，当没有填写自定义错误消息时
+     *    // 提示为 username不能为空
+     *    // 当前设置别名之后
+     *    // 提示为 用户名不能为空
+     *    alias: '用户名',
+     *
+     *    // 验证前置
+     *    // val 当前字段值
+     *    // data 所有数据
+     *    onbefore: function(val, data){
+     *        return val + 'abc';
+     *    },
+     *    exist: false,
+     *    trim: true,
+     *
+     *    // 验证规则
+     *    required: true,
+     *    length: 10,
+     *    minLength: 4,
+     *    maxLength: 12,
+     *    bytes: 10,
+     *    minBytes: 4,
+     *    maxBytes: 12,
+     *    min: 4,
+     *    max: 4,
+     *    regexp: /^[a-z]\w{3,11}$/,
+     *    equal: 'cloudcome',
+     *    inArray: ['cloudcome', 'yundanran'],
+     *    // val 当前字段值
+     *    // [data 所有数据] 可选
+     *    // next 执行下一步
+     *    function: function(val, next){
+     *        // 这里可以是异步的
+     *        // 比如远程校验可以写在这里
+     *        ajax.post('./validate.json', {
+     *            username: val
+     *        }).on('success', function(json){
+     *            if(json.code>0){
+     *                next();
+     *            }else{
+     *                next(new Error(json.msg));
+     *            }
+     *        }).on('error', function(){
+     *            next(new Error('网络连接错误，验证失败'));
+     *        });
+     *    },
+     *
+     *    // 验证消息
+     *    msg: {
+     *        required: '用户名不能为空',
+     *        length: '用户名长度必须为10'
+     *        // 未自定义的消息，以默认输出
+     *        // 每个验证规则都必须配备一个消息体，
+     *        // 除了自定义的`function`
+     *    },
+     *
+     *    // 验证后置
+     *    onafter: function(val){
+     *        return val + 'abc';
+     *    }
+     * });
      */
     pushRule: function (rule, isOverride) {
         var the = this;
